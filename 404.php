42229f6edabc102d9c95f9d4b239a7f01eff7c0e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-16 17:34:19 +0900

    Add: 404.php | PHPDoc

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+/**	op-asset-template:/404.php
+ *
+ * @created    2025-10-16
+ * @version    1.0
+ * @package    op-asset-template
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
