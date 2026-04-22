75c73ee94e41693734174643d7712ccdbe6e7fd3
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-22 16:38:23 +0900

    Doc: * | PHPDoc

--- 404.php
+++ 404.php
@@ -2,10 +2,9 @@
 /**	op-asset-template:/404.php
  *
  * @created    2025-10-16
- * @version    1.0
+ * @license    Apache-2.0
  * @package    op-asset-template
- * @author     Tomoaki Nagahara
- * @copyright  Tomoaki Nagahara All right reserved.
+ * @copyright  Tomoaki Nagahara
  */
 
 /**	namespace
